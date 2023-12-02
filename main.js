321fc6da03d10f4bb810c112cce9491a61bcafb8
Yasmim <105066319+yasmim-lima@users.noreply.github.com>
2023-12-02 14:27:07 -0300

    Adiciona funcao palavra aleatoria

---
+++ main.js
@@ -0,0 +1,10 @@
+let palavras = ["pedra", "papel", "tesoura"]
+
+function palavraAleatoria (palavras) {
+    let indice = Math.floor(Math.random() * palavras.length)
+    return palavras[indice]
+}
+
+function umaRodada (escolhaJogador, escolhaComputador) {
+
+}
