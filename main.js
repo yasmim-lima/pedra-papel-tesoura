723dbd3a1a51f5df35e5674de594988fa6e9c0da
Yasmim <105066319+yasmim-lima@users.noreply.github.com>
2023-12-04 20:07:39 -0300

    Verifica quem ganhou

--- main.js
+++ main.js
@@ -1,36 +1,58 @@
-function palavraAleatoria (palavras) {
+function palavraAleatoria(palavras) {
     let indice = Math.floor(Math.random() * palavras.length);
     return palavras[indice];
 }
 
-function umaRodada (escolhaJogador, escolhaComputador) {
-        if (escolhaJogador == "pedra" && escolhaComputador == "tesoura"){
-            return "Voce ganhou! Pedra vence tesoura!";
-        } else if (escolhaJogador == "pedra" && escolhaComputador == "papel"){
-            return "Voce perdeu! Papel vence pedra!";
-        } else if (escolhaJogador == "tesoura" && escolhaComputador == "pedra"){
-            return "Voce perdeu! Pedra vence tesoura!";
-        } else if (escolhaJogador == "tesoura" && escolhaComputador == "papel"){
-            return "Voce ganhou! Tesoura vence papel!";
-        } else if (escolhaJogador == "papel" && escolhaComputador == "pedra"){
-            return "Voce ganhou! Papel vence pedra!";
-        } else if (escolhaJogador == "papel" && escolhaComputador == "tesoura"){
-            return "Voce perdeu! Tesoura vence papel!";
-        } else if (escolhaJogador === escolhaComputador) {
-            return "Houve um empate! Jogue novamente";
-        } else {
-            return "Escolha inválida";
-        }
-}
-
-function jogo() {
-    for (let i = 0; i < 5; i++){
-        const palavraJogador = prompt("Pedra, papel ou tesoura?").toLowerCase();
-        const palavraComputador = palavraAleatoria(palavras);
-        alert(umaRodada(palavraJogador, palavraComputador));
+function umaRodada(escolhaJogador, escolhaComputador) {
+    if (escolhaJogador == "pedra" && escolhaComputador == "tesoura") {
+        return "Voce ganhou! Pedra vence tesoura!";
+    } else if (escolhaJogador == "pedra" && escolhaComputador == "papel") {
+        return "Voce perdeu! Papel vence pedra!";
+    } else if (escolhaJogador == "tesoura" && escolhaComputador == "pedra") {
+        return "Voce perdeu! Pedra vence tesoura!";
+    } else if (escolhaJogador == "tesoura" && escolhaComputador == "papel") {
+        return "Voce ganhou! Tesoura vence papel!";
+    } else if (escolhaJogador == "papel" && escolhaComputador == "pedra") {
+        return "Voce ganhou! Papel vence pedra!";
+    } else if (escolhaJogador == "papel" && escolhaComputador == "tesoura") {
+        return "Voce perdeu! Tesoura vence papel!";
+    } else if (escolhaJogador === escolhaComputador) {
+        return "Houve um empate! Jogue novamente";
+    } else {
+        return "Escolha inválida";
     }
 }
 
+function jogo() {
+    let palavras = ["pedra", "papel", "tesoura"];
+    let vitoriasJogador = 0;
+    let vitoriasComputador = 0;
 
-let palavras = ["pedra", "papel", "tesoura"];
-jogo ();
+    for (let i = 0; i < 5; ) {
+        const palavraJogador = prompt("Pedra, papel ou tesoura?").toLowerCase();
+
+        if (palavras.includes(palavraJogador)) {
+            const palavraComputador = palavraAleatoria(palavras);
+            const resultado = umaRodada(palavraJogador, palavraComputador);
+
+            if (resultado.includes("ganhou")) {
+                vitoriasJogador++;
+            } else if (resultado.includes("perdeu")) {
+                vitoriasComputador++;
+            }
+
+            alert(resultado);
+            i++;  
+        } else {
+            alert("Escolha inválida. Tente novamente.");
+        }
+    }
+
+    if (vitoriasJogador > vitoriasComputador) {
+        alert("Você ganhou!");
+    } else if (vitoriasComputador > vitoriasJogador) {
+        alert("O computador ganhou!");
+    }
+}
+
+jogo();
