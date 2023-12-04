362ed46a1fb5af4aed02f9ae3290c04ae6dee039
Yasmim <105066319+yasmim-lima@users.noreply.github.com>
2023-12-04 19:04:10 -0300

    Adiciona funcao para reproduzir uma rodada

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-let palavras = ["pedra", "papel", "tesoura"]
-
 function palavraAleatoria (palavras) {
-    let indice = Math.floor(Math.random() * palavras.length)
-    return palavras[indice]
+    let indice = Math.floor(Math.random() * palavras.length);
+    return palavras[indice];
 }
 
 function umaRodada (escolhaJogador, escolhaComputador) {
-
+        if (escolhaJogador == "pedra" && escolhaComputador == "tesoura"){
+            return "Voce ganhou! Pedra vence tesoura!";
+        } else if (escolhaJogador == "pedra" && escolhaComputador == "papel"){
+            return "Voce perdeu! Papel vence pedra!";
+        } else if (escolhaJogador == "tesoura" && escolhaComputador == "pedra"){
+            return "Voce perdeu! Pedra vence tesoura!";
+        } else if (escolhaJogador == "tesoura" && escolhaComputador == "papel"){
+            return "Voce ganhou! Tesoura vence papel!";
+        } else if (escolhaJogador == "papel" && escolhaComputador == "pedra"){
+            return "Voce ganhou! Papel vence pedra!";
+        } else if (escolhaJogador == "papel" && escolhaComputador == "tesoura"){
+            return "Voce perdeu! Tesoura vence papel!";
+        } else {
+            return "Houve um empate! Jogue novamente"
+        }
 }
+
+
+let palavras = ["pedra", "papel", "tesoura"];
+let palavraComputador = palavraAleatoria(palavras);
+let palavraJogador = "pedra";
+palavraJogador = palavraJogador.toLowerCase();
+console.log("Sua escolha: " + palavraJogador);
+console.log("Escolha do computador: " + palavraComputador);
+console.log(umaRodada(palavraJogador, palavraComputador));
